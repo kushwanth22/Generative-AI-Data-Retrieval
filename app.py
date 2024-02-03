8ce3d9b11ebbdb6289e55d56012f3d2c41acb429
Kushwanth Chowday Kandala <kushwanthkandala@gmail.com>
2024-02-03 04:12:53 -0800

    update app.py

--- app.py
+++ app.py
@@ -23,10 +23,34 @@ import torch
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 if device != 'cuda':
-    st.title(f"you are using {device}. This is much slower than using "
+    st.text(f"you are using {device}. This is much slower than using "
     "a CUDA-enabled GPU. If on colab you can chnage this by "
     "clicking Runtime > change runtime type > GPU.")
 
 model = SentenceTransformer("all-MiniLM-L6-v2", device=device)
+st.divider()
 
 # Creating a Index(Pinecone Vector Database)
+def chat_actions():
+    st.session_state["chat_history"].append(
+        {"role": "user", "content": st.session_state["chat_input"]},
+    )
+
+    response = model.generate_content(st.session_state["chat_input"])
+    st.session_state["chat_history"].append(
+        {
+            "role": "assistant",
+            "content": response.text,
+        },  # This can be replaced with your chat response logic
+    )
+
+
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
+
+
+st.chat_input("Enter your message", on_submit=chat_actions, key="chat_input")
+
+for i in st.session_state["chat_history"]:
+    with st.chat_message(name=i["role"]):
+        st.write(i["content"])
