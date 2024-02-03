9eb3e785630ad4c9bf9aa60cc6b3f74474873e7b
Kushwanth Chowday Kandala <kushwanthkandala@gmail.com>
2024-02-03 04:55:18 -0800

    update app.py with pinecone connection

--- app.py
+++ app.py
@@ -31,12 +31,50 @@ model = SentenceTransformer("all-MiniLM-L6-v2", device=device)
 st.divider()
 
 # Creating a Index(Pinecone Vector Database)
+import os
+import pinecone
+
+from pinecone import Index, GRPCIndex
+
+PINECONE_API_KEY=os.getenv("PINECONE_API_KEY")
+PINECONE_ENV=os.getenv("PINECONE_ENV")
+PINECONE_ENVIRONMENT=os.getenv("PINECONE_ENVIRONMENT")
+
+def connect_pinecone():
+    pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENV)
+    st.code(pinecone)
+    st.divider()
+    st.text(pinecone.list_indexes())
+    st.divider()
+    st.text(f"Succesfully connected to the pinecone")
+    return pinecone
+
+def get_pinecone_semantic_index(pinecone):
+    index_name = "sematic-search"
+
+    # only create if it deosnot exists
+    if index_name not in pinecone.list_indexes():
+        pinecone.create_index(
+            name=index_name,
+            description="Semantic search",
+            dimension=model.get_sentence_embedding_dimension(),
+            metric="cosine",
+        )
+    # now connect to index
+    index = pinecone.GRPCIndex(index_name)
+    st.text(f"Succesfully connected to the pinecone")
+    return index
+
 def chat_actions():
+    
+    pinecone = connect_pinecone()
+    index = get_pinecone_semantic_index(pinecone semantic index)
+
     st.session_state["chat_history"].append(
         {"role": "user", "content": st.session_state["chat_input"]},
     )
 
-    response = model.generate_content(st.session_state["chat_input"])
+    response = model.encode(st.session_state["chat_input"])
     st.session_state["chat_history"].append(
         {
             "role": "assistant",
@@ -54,3 +92,18 @@ st.chat_input("Enter your message", on_submit=chat_actions, key="chat_input")
 for i in st.session_state["chat_history"]:
     with st.chat_message(name=i["role"]):
         st.write(i["content"])
+
+### Creating a Index(Pinecone Vector Database)
+# %%writefile .env
+PINECONE_API_KEY=os.getenv("PINECONE_API_KEY")
+PINECONE_ENV=os.getenv("PINECONE_ENV")
+PINECONE_ENVIRONMENT=os.getenv("PINECONE_ENVIRONMENT")
+
+import os
+import pinecone
+
+from pinecone import Index, GRPCIndex
+pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENV)
+st.text(pinecone)
+
+
